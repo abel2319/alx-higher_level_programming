2ad65b894a779455eac0a9fe04ba1eb074d197e4
abel2319 <abelbakary@gmail.com>
2022-09-03 03:26:08 +0100

    task10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dict = a_dictionary.copy()
+    if new_dict != {}:
+        key_list = new_dict.keys()
+        j = key_list[0]
+        for i in new_dict.keys():
+            if new_dict[i] >= new_dict[j]:
+                j = i
+        return (j)
+    return (None)
