c6aac5f83780bf2c4ccdfaf8a7fe1d9c5270f35c
abel2319 <abelbakary@gmail.com>
2022-09-03 04:23:59 +0100

    task10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
 
     best_score = None
 
-    if a_dictionary != None:
+    if a_dictionary is not None:
         j = 0
         for i in a_dictionary:
             if a_dictionary[i] > j:
