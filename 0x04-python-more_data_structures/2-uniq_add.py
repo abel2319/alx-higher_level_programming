fa04fc94ea0a4405d30cc0c7575c42312c9a2668
abel2319 <abelbakary@gmail.com>
2022-09-03 03:55:17 +0100

    task2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     new_list = []
+    rtn = 0
     if my_list != []:
         new_list = [i for i in my_list]
         new_list.sort()
@@ -8,4 +9,4 @@ def uniq_add(my_list=[]):
         for i in range(1, len(new_list)):
             if new_list[i] != new_list[i - 1]:
                 rtn += new_list[i]
-    return (0)
+    return (rtn)
