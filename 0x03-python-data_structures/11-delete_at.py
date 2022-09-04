cc3b4472e277e58cab7754efc40ba2b51299eda9
abel2319 <abelbakary@gmail.com>
2022-09-04 09:11:33 +0100

    correction task7-11

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx=0):
     tmp = []
-    if idx < 0 or idx >= len(my_list):
-        return (my_list)
-    for i in my_list:
-        if i != my_list[idx]:
-            tmp.append(i)
-    my_list = tmp
+    if idx >= 0 or idx < len(my_list):
+        del my_list[idx]
     return (my_list)
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "" or sentence == None:
+        return (None, 0)
+    return (sentence[0], len(senetence))
