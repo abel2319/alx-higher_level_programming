8ca627d3c4d8e656a54421431128681870e22ac2
abel2319 <abelbakary@gmail.com>
2022-09-12 11:49:57 +0100

    task1-6

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,6 +7,5 @@ def safe_print_list(my_list=[], x=0):
             nbr += 1
         except IndexError:
             break
-        finally:
-            print("")
-            return (nbr)
+    print("")
+    return (nbr)
