d1d577816093aa12978ee657a494ac34df80bbd0
abel2319 <abelbakary@gmail.com>
2022-09-27 20:43:26 +0100

    task 0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     Args:
         filename (str): name of the file
     '''
-    with open(filename, emcoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
