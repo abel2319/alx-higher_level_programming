43936836fcd2bc03cb4c5b430db90469ad9703be
abel2319 <abelbakary@gmail.com>
2022-09-27 20:42:23 +0100

    task 0 correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''this module open and read a file'''
 
+
 def read_file(filename=""):
     '''Functionthat reads into a file
     Args:
         filename (str): name of the file
     '''
-    with open(filename) as f:
-            print(f.read())
+    with open(filename, emcoding="utf-8") as f:
+        print(f.read(), end="")
