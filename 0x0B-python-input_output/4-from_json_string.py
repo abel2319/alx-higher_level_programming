02f9c7b6c1746b8efab3a72f7c84ac5fa6a1c890
abel2319 <abelbakary@gmail.com>
2022-09-28 06:40:22 +0100

    task 3-6

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,10 +3,10 @@
 import json
 
 
-def to_json_string(my_obj):
+def from_json_string(my_str):
     '''gives the python representation of a json object
     Args:
         my_obj (obj): object
     Return: the python representation of my_obj
     '''
-    return (json_loads(my_obj))
+    return (json_loads(my_str))
