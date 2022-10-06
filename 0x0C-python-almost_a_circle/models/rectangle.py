12d9a9128de81a85d96d6825200332890816eea0
abel2319 <abelbakary@gmail.com>
2022-10-07 00:01:04 +0100

    task8

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -135,22 +135,22 @@ class Rectangle(Base):
 
         elif len(args) == 2:
             self.id = args[0]
-            self.width(args[1])
+            self.width = args[1]
 
         elif len(args) == 3:
             self.id = args[0]
-            self.width(args[1])
-            self.height(args[2])
+            self.width = args[1]
+            self.height = args[2]
 
         elif len(args) == 4:
             self.id = args[0]
-            self.width(args[1])
-            self.height(args[2])
-            self.x(args[3])
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
 
         elif len(args) == 5:
             self.id = args[0]
-            self.width(args[1])
-            self.height(args[2])
-            self.x(args[3])
-            self.y(args[4])
+            self.width = args[1]
+            self.height = args[2]
+            self.x = args[3]
+            self.y = args[4]
