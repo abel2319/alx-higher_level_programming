e6337deea94c3eb76fd0ce71cf5b99bee3fcad71
abel2319 <abelbakary@gmail.com>
2022-10-06 10:50:40 +0100

    task1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-
+'''Module that defines the base'''
 
 class Base:
-    
+    '''class Base
+    Args:
+        nb_objects (int): number of instances created
+    '''
     __nb_objects = 0
 
     def __init__(self, id=None):
-        ''''''
+        '''Initialization
+        Args:
+            id (int): the id of the new instance
+        '''
         if id is not None:
             self.id = id
         else:
