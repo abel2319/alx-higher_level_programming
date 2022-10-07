e30a2cfb3cc7ea360ebe074ce8c672d7dfaeee1b
abel2319 <abelbakary@gmail.com>
2022-10-07 23:34:34 +0100

    task15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Module that defines the base'''
+import json
 
 
 class Base:
@@ -19,3 +20,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation of list_dictionaries'''
+        return (json.dumps(list_dictionaries))
