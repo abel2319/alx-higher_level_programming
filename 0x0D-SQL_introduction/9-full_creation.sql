7f7070328be6239aedbf140c60a7be7ef297d52a
abel2319 <abelbakary@gmail.com>
2022-11-02 10:16:58 -0700

    essaie

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+---script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE `second_table`(
+	id int,
+	name varchar(256)
+	score int
+);
