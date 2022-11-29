942f3e31b99e75daedc23a2865ec153ee18f0941
abel2319 <abelbakary@gmail.com>
2022-11-29 03:13:13 +0100

    task6.4

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-tab = ["C is fun", "Python is cool", "JavaScript is amazing"]
+const tab = ["C is fun", "Python is cool", "JavaScript is amazing"]
 for(let i = 0; i < 3; i++){
 	console.log(tab[i]);
 }
