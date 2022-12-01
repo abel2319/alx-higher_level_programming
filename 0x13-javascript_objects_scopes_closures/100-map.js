943538decaca2c98677965dce16e300d40efe7b7
abel2319 <abelbakary@gmail.com>
2022-12-02 00:40:07 +0100

    task11

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const list = require('./100-data').list;
+
 console.log(list);
-console.log(list.map(({i}, index) => {
-  i * index
-}));
+console.log(list.map((i, index) => i * index));
