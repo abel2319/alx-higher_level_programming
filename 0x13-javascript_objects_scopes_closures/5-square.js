1d4a2dcd017046da7517a4061c931ff7ba045241
abel2319 <abelbakary@gmail.com>
2022-12-01 15:36:05 +0100

    semistandard

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -3,7 +3,5 @@
 const Rectangle = require('./4-rectangle');
 
 module.exports = class Square extends Rectangle {
-	constructor(size){
-		super(size, size);
-	}
+  constructor (size) { super(size, size); }
 };
