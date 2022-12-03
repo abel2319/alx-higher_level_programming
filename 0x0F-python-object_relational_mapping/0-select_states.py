970e9dea96e160df901b80b34e5f22ff788a9fdc
abel2319 <abelbakary@gmail.com>
2022-12-03 11:37:01 +0100

    task0.4

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
                          passwd=sys.argv[2],
                          db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM `states`")
     (print(state) for state in c.fetchall())
