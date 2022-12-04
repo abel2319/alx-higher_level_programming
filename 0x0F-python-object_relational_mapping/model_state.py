251ef2b38d85729cbc82bfa983b8d71f99f81b4d
abel2319 <abelbakary@gmail.com>
2022-12-04 15:25:58 +0100

    task6.1

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,14 +2,21 @@
 """
 the class definition of a State and an instance Base = declarative_base()
 """
+
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
 
+
 class State(Base):
     """Class States for database
+    Args:
+        __tablename__ : name of the table
+        id: id of each state
+        name: name of each state
     """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
