cae57c335c77c65e1faac45d5dfc9fcbe604e7d9
abel2319 <abelbakary@gmail.com>
2022-12-29 18:15:12 +0100

    task9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import json
+
+    token = argv[2]
+    headers = {'Authorization': 'token ' + token}
+    login = requests.get('https://api.github.com/' + argv[1], headers=headers)
+
+    if 'application/json' in login.headers.get('Content-Type', ''):
+        res = login.json()
+        if res != {}:
+            print("{}".format(res['id']))
+        else:
+            print('No result')
+    else:
+        print('Not a valid JSON')
