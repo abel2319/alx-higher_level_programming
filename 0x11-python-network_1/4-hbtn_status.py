0a0ef4895b8194f039091be48eacdd860121fe1a
abel2319 <abelbakary@gmail.com>
2022-12-29 15:10:56 +0100

    task4

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib"""
+
+
+if __name__ == "__main__":
+    import requests
+    response = requests.get('https://alx-intranet.hbtn.io/status')
+    #html = response.read()
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
