2ee779ec5d27e55ba082ccae10b4fcbccab7d5de
abel2319 <abelbakary@gmail.com>
2022-12-29 17:24:12 +0100

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a Python script that fetches https://alx-intranet.hbtn.io/status
+You must use the package urllib"""
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    import json
+
+    q = ""
+    if len(argv) > 1:
+        q = argv[1]
+
+    response = requests.post('http://0.0.0.0:5000/search_user',\
+                        data=json.dumps({'q': q}))
+    if 'application/json' in response.headers.get('Content-Type', ''):
+        res = response.json()
+        if res is not None:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+        else:
+            print('No result')
+    else:
+        print('Not a valide JSON')
