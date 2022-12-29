0f8a37944e791f90cd689cd384f636f2f49acec6
abel2319 <abelbakary@gmail.com>
2022-12-29 16:49:10 +0100

    task7.1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Python script that takes in a URL, sends a request to
-the URL and displays the body of the response (decoded in utf-8)."""
+the URL and displays the body of the response."""
 
 
 if __name__ == "__main__":
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     from requests
 
     req = requests(argv[1])
-    try:
-        req = requests(argv[1])
-        print(req.text)
-    except:
+    req = requests(argv[1])
+    if req.raise_for_status is not None:
         print('Error code: {}'.format(req.status_code))
+    else:
+        print(req.text)
