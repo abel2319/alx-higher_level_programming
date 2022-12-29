372c2dfe7dd5b2e347fab3f65eb13bb3b4347af5
abel2319 <abelbakary@gmail.com>
2022-12-29 15:01:10 +0100

    task3.3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,11 +6,11 @@ the URL and displays the body of the response (decoded in utf-8)."""
 if __name__ == "__main__":
     from sys import argv
     from urllib.request import Request, urlopen
-    from urllib.error import URLError, HTTPError
+    from urllib.error import HTTPError
 
     req = Request(argv[1])
     try:
         with urlopen(req) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as e:
-        print('Error code: ', e.code)
+        print('Error code: {}'.format(e.code))
