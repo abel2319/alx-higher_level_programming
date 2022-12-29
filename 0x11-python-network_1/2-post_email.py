eb310ae4ced839d66a330f42e43361a917b13709
abel2319 <abelbakary@gmail.com>
2022-12-29 08:53:08 +0100

    task2.2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
     import urllib.request
     import urllib.parse
     from sys import argv
-    
+
     email = {'email': argv[2]}
 
     data = urllib.parse.urlencode(email)
     data = data.encode('ascii')
-    
+
     req = urllib.request.Request(argv[1], data)
     with urllib.request.urlopen(req) as response:
         the_page = response.read()
+        print(the_page)
