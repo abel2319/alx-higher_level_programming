8e5bff02381804f0e427fcf4d4bd52b4434ba17c
abel2319 <abelbakary@gmail.com>
2023-01-17 23:27:21 +0100

    task2.1

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-request(process.argv[1], function (error, response, body) {
-  if (error) console.error('error:', error);
-  console.log('code:', response && response.statusCode);
+request(process.argv[2], (err, data) => {
+  if (err) console.error('error:', err);
+  console.log(`code: ${data.statusCode}`);
 });
