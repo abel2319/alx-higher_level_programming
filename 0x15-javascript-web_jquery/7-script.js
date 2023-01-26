2adc0afd83e388fe2873c6f3fdbb7888619cf940
abel2319 <abelbakary@gmail.com>
2023-01-26 05:07:51 +0100

    task7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,6 @@
+const $ = window.jQuery;
+$(function () {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data, textStatus) {
+      $('DIV#character').text(data.name);
+  });
+});
